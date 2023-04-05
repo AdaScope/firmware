fbdcb178ac28962b908d82118d0f3228312df288
JSIT-20 <jdeme051@uottawa.ca>
2023-04-05 15:26:26 -0400

    UInt to characters

--- src/adc_standalone.adb
+++ src/adc_standalone.adb
@@ -7,10 +7,13 @@ with STM32.Device; use STM32.Device;
 
 with STM32.GPIO; use STM32.GPIO;
 with STM32.SPI;  use STM32.SPI;
+with STM32.USARTs;  use STM32.USARTs;
 
 with LCD_Std_Out;
 with HAL.Framebuffer; use HAL.Framebuffer;
 with BMP_Fonts; use BMP_Fonts;
+
+with HAL;           use HAL;
 
 with STM32.Board;
 
@@ -26,6 +29,7 @@ with Uart_For_Board;
 with Simple_Adc;
 with adc;
 
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
 with Textures.Autosar;
 
@@ -33,6 +37,77 @@ procedure Adc_Standalone is
 
    Period : constant Time_Span := Milliseconds (250);  -- arbitrary
    Next_Release : Time := Clock;
+   temp : Unbounded_String;
+   TX_Pin : constant GPIO_Point := PA9;
+   RX_Pin : constant GPIO_Point := PA10;
+
+
+   procedure Initialize_UART_GPIO;
+
+   procedure Initialize;
+
+   procedure Await_Send_Ready (This : USART) with Inline;
+
+   procedure Put_Blocking (This : in out USART;  Data : UInt16);
+
+   --------------------------
+   -- Initialize_UART_GPIO --
+   --------------------------
+
+   procedure Initialize_UART_GPIO is
+   begin
+      Enable_Clock (USART_1);
+      Enable_Clock (RX_Pin & TX_Pin);
+
+      Configure_IO
+        (RX_Pin & TX_Pin,
+         (Mode           => Mode_AF,
+          AF             => GPIO_AF_USART1_7,
+          Resistors      => Pull_Up,
+          AF_Speed       => Speed_50MHz,
+          AF_Output_Type => Push_Pull));
+   end Initialize_UART_GPIO;
+
+   ----------------
+   -- Initialize --
+   ----------------
+
+   procedure Initialize is
+   begin
+      Initialize_UART_GPIO;
+
+      Disable (USART_1);
+
+      Set_Baud_Rate    (USART_1, 115_200);
+      Set_Mode         (USART_1, Tx_Rx_Mode);
+      Set_Stop_Bits    (USART_1, Stopbits_1);
+      Set_Word_Length  (USART_1, Word_Length_8);
+      Set_Parity       (USART_1, No_Parity);
+      Set_Flow_Control (USART_1, No_Flow_Control);
+
+      Enable (USART_1);
+   end Initialize;
+
+   ----------------------
+   -- Await_Send_Ready --
+   ----------------------
+
+   procedure Await_Send_Ready (This : USART) is
+   begin
+      loop
+         exit when Tx_Ready (This);
+      end loop;
+   end Await_Send_Ready;
+
+   ------------------
+   -- Put_Blocking --
+   ------------------
+
+   procedure Put_Blocking (This : in out USART;  Data : UInt16) is
+   begin
+      Await_Send_Ready (This);
+      Transmit (This, UInt9 (Data));
+   end Put_Blocking;
 
    procedure Log_To_LCD (Voltage : String) is
       Buf : HAL.Bitmap.Bitmap_Buffer'Class := STM32.Board.Display.Hidden_Buffer (1).all;
@@ -67,7 +142,13 @@ procedure Adc_Standalone is
         Value : aliased Simple_Adc.Data_T := 0;
         Result : Simple_Adc.Status_T := Read_ADC_Value (1, Value'Unchecked_Access);
     begin
+        
         Log_To_LCD ("Voltage:" & Value'Image & " mV");
+        temp := To_Unbounded_String (Value'Image);
+        for i in 2 .. Length(temp) loop
+          Put_Blocking(USART_1, Character'Pos(Element(temp, i)));
+         end loop;
+        Put_Blocking(USART_1, Character'Pos(ASCII.LF));
     end;
 
 begin
@@ -75,13 +156,14 @@ begin
 
    adc.Init_ADC;
    Simple_Adc.Start_Group_Conversion (1);
+   Initialize;
 
 
    loop
-      --Read_Adc;
-      Uart_For_Board;
-      Next_Release := Next_Release + Period;
-      delay until Next_Release;
+      Read_Adc;
+      --Uart_For_Board;
+      --Next_Release := Next_Release + Period;
+      --delay until Next_Release;
    end loop;
 
 end Adc_Standalone;
