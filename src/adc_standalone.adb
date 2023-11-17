19f3885fc1c4eba34f14987fb8bcab8d1323909f
Florian Koudjonou <floriankoud@hotmail.com>
2023-11-17 01:16:07 -0500

    Fixed some styling warnings

--- src/adc_standalone.adb
+++ src/adc_standalone.adb
@@ -24,7 +24,7 @@ with Ada.Characters.Latin_1; use Ada.Characters.Latin_1;
 
 with Uart_For_Board;
 with Simple_Adc;
-with adc;
+with Adc;
 
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
@@ -34,68 +34,74 @@ with Min_Ada;
 
 procedure Adc_Standalone is
 
-   Period : constant Time_Span := Milliseconds (250);  -- arbitrary
-   Next_Release : Time := Clock;
-   temp : Unbounded_String;
-   Context : Min_Ada.Min_Context;
-   Payload : Min_Ada.Min_Payload;
+   Period        : constant Time_Span := Milliseconds (250);  -- arbitrary
+   Next_Release  : Time := Clock;
+   Temp          : Unbounded_String;
+   Context       : Min_Ada.Min_Context;
+   Payload       : Min_Ada.Min_Payload;
    Payload_Index : Integer;
-   Data_Count : Integer;
+   Data_Count    : Integer;
 
-   function Read_ADC_Value (G : Simple_Adc.Group_T; 
-                           V : access Simple_Adc.Data_T) return Simple_Adc.Status_T
+   function Read_ADC_Value (
+      G : Simple_Adc.Group_T;
+      V : access Simple_Adc.Data_T
+   ) return Simple_Adc.Status_T
    with
       Import        => True,
       Convention    => C,
       External_Name => "Adc_ReadGroup";
-   Value : aliased Simple_Adc.Data_T := 0;
+
+   Value  : aliased Simple_Adc.Data_T := 0;
    Result : Simple_Adc.Status_T;
 
-    procedure Read_Adc is
+   procedure Read_Adc is
+   begin
 
-
-    begin
-        
-        temp := To_Unbounded_String (Value'Image);
-        for i in 2 .. Length(temp) loop
-          Uart_For_Board.Put_Blocking(USART_1, Character'Pos(Element(temp, i)));
-         end loop;
-        Uart_For_Board.Put_Blocking(USART_1, Character'Pos(ASCII.LF));
-    end;
-
+      Temp := To_Unbounded_String (Value'Image);
+      for I in 2 .. Length (Temp) loop
+         Uart_For_Board.Put_Blocking (
+            USART_1,
+            Character'Pos (Element (Temp, I))
+      );
+      end loop;
+      Uart_For_Board.Put_Blocking (
+         USART_1,
+         Character'Pos (ASCII.LF)
+      );
+   end Read_Adc;
 begin
-
-   adc.Init_ADC;
+   Adc.Init_ADC;
    Simple_Adc.Start_Group_Conversion (1);
    Uart_For_Board.Initialize;
 
-   --Init min
+   --  Init min
    Min_Ada.Min_Init_Context (Context);
    Payload_Index := 1;
-   Data_Count := 0; --Max of 51 for now
+   Data_Count := 0; -- Max of 51 for now
 
    loop
       while Data_Count < 51 loop
          Result := Read_ADC_Value (1, Value'Unchecked_Access);
-         temp := To_Unbounded_String (Value'Image);
-         for i in 2 .. Length(temp) loop
-            Payload(Min_Ada.Byte (Payload_Index)) := Min_Ada.Byte (Character'Pos(Element(temp, i)));
-            Payload_Index := Payload_Index + 1;
+         Temp := To_Unbounded_String (Value'Image);
+         for I in 2 .. Length (Temp) loop
+            Payload (Min_Ada.Byte (Payload_Index)) :=
+               Min_Ada.Byte (Character'Pos (Element (Temp, I)));
+                  Payload_Index := Payload_Index + 1;
          end loop;
-         Payload(Min_Ada.Byte (Payload_Index)) := Min_Ada.Byte (Character'Pos(ASCII.LF));
-         Payload_Index := Payload_Index + 1;
+         Payload (Min_Ada.Byte (Payload_Index)) :=
+            Min_Ada.Byte (Character'Pos (ASCII.LF));
+               Payload_Index := Payload_Index + 1;
          Data_Count := Data_Count + 1;
       end loop;
       Min_Ada.Send_Frame (
          Context => Context,
-         ID => 5,
+         ID => 1,
          Payload => Payload,
          Payload_Length => Min_Ada.Byte (Payload_Index - 1)
       );
 
       Data_Count := 0;
       Payload_Index := 1;
-
    end loop;
 
 end Adc_Standalone;
