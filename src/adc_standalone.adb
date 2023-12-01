7934db72ed78a522cf9cd26461544da1f255a691
JSIT-20 <vboxuser@Ubuntu22.04.1.myguest.virtualbox.org>
2023-12-01 15:35:45 -0500

    Use new data structure and refactor

--- src/adc_standalone.adb
+++ src/adc_standalone.adb
@@ -52,8 +52,63 @@ procedure Adc_Standalone is
    Payloads        : Payload_Arr;
    Payload_Indexes : array (1 .. Frame_Count) of Integer;
 
-   Value  : Beta_Types.UInt32 := 0;
+   D : constant := 0.1;
+   type ADC_Reading is delta D range 0.0 .. 3000.0;
+
+   type ADC_Reading_Bytes is array (1 .. 2) of Min_Ada.Byte;
+
+   Value            : ADC_Reading;
+   Value_Bytes      : ADC_Reading_Bytes with Address => Value'Address;
+
+   --Value  : Beta_Types.UInt32 := 0;
    --Result : Simple_Adc.Status_T;
+
+   procedure Collect_Data(Input : Integer) is
+   begin
+      Frame_Index := 1;
+      --  Iterate through all the frames
+      while Frame_Index < Frame_Count + 1 loop
+
+         --  Iterate through all the data points
+         while Data_Count < Data_Points_Per_Payload loop
+            Value := ADC_Reading (Adc.Read_Group (Input));
+            Payloads (Frame_Index) (Min_Ada.Byte (Payload_Index)) := 
+               Value_Bytes (2);
+            Payload_Index := Payload_Index + 1;
+            Payloads (Frame_Index) (Min_Ada.Byte (Payload_Index)) := 
+               Value_Bytes (1);
+            Payload_Index := Payload_Index + 1;
+            Data_Count := Data_Count + 1;
+         end loop;
+         Payload_Indexes (Frame_Index) := Payload_Index;
+         Frame_Index := Frame_Index + 1;
+         Data_Count := 0;
+         Payload_Index := 1;
+      end loop;
+   end Collect_Data;
+
+   procedure Send_Data(Input : Integer) is
+   begin
+      Frame_Index := 1;
+      while Frame_Index < Frame_Count + 1 loop
+         if Frame_Index = 1 then
+            Min_Ada.Send_Frame (
+               Context => Context,
+               ID => Min_Ada.App_ID (Input + 4),
+               Payload => Payloads(Frame_Index),
+               Payload_Length => Min_Ada.Byte (Payload_Indexes(Frame_Index) - 1)
+            );
+         else
+            Min_Ada.Send_Frame (
+               Context => Context,
+               ID => Min_Ada.App_ID (Input),
+               Payload => Payloads(Frame_Index),
+               Payload_Length => Min_Ada.Byte (Payload_Indexes(Frame_Index) - 1)
+            );
+         end if;
+         Frame_Index := Frame_Index + 1;
+      end loop;
+   end Send_Data;
 
 begin
    Adc.Init_ADC;
@@ -66,96 +121,12 @@ begin
    Frame_Index := 1;
 
    loop
-      --  Iterate through all the frames
-      while Frame_Index < Frame_Count + 1 loop
 
-         --  Iterate through all the data points
-         while Data_Count < Data_Points_Per_Payload loop
-            Value := Adc.Read_Group (1);
-            Temp := To_Unbounded_String (Value'Image);
-            for I in 2 .. Length (Temp) loop
-               Payloads (Frame_Index) (Min_Ada.Byte (Payload_Index)) :=
-                  Min_Ada.Byte (Character'Pos (Element (Temp, I)));
-               Payload_Index := Payload_Index + 1;
-            end loop;
-            Payloads (Frame_Index) (Min_Ada.Byte (Payload_Index)) :=
-               Min_Ada.Byte (Character'Pos (ASCII.LF));
-            Payload_Index := Payload_Index + 1;
-            Data_Count := Data_Count + 1;
-         end loop;
-         Payload_Indexes (Frame_Index) := Payload_Index;
-         Frame_Index := Frame_Index + 1;
-         Data_Count := 0;
-         Payload_Index := 1;
-      end loop;
+      Collect_Data(1);
+      Send_Data(1);
+      Collect_Data(2);
+      Send_Data(2);
 
-      --Send 10 frames
-      Frame_Index := 1;
-      while Frame_Index < Frame_Count + 1 loop
-         if Frame_Index = 1 then
-            Min_Ada.Send_Frame (
-               Context => Context,
-               ID => 5,
-               Payload => Payloads(Frame_Index),
-               Payload_Length => Min_Ada.Byte (Payload_Indexes(Frame_Index) - 1)
-            );
-         else
-            Min_Ada.Send_Frame (
-               Context => Context,
-               ID => 1,
-               Payload => Payloads(Frame_Index),
-               Payload_Length => Min_Ada.Byte (Payload_Indexes(Frame_Index) - 1)
-            );
-         end if;
-         Frame_Index := Frame_Index + 1;
-      end loop;
-
-      Frame_Index := 1;
-
-      while Frame_Index < Frame_Count + 1 loop
-
-         --  Iterate through all the data points
-         while Data_Count < Data_Points_Per_Payload loop
-            Value := Adc.Read_Group (2);
-            Temp := To_Unbounded_String (Value'Image);
-            for I in 2 .. Length (Temp) loop
-               Payloads (Frame_Index) (Min_Ada.Byte (Payload_Index)) :=
-                  Min_Ada.Byte (Character'Pos (Element (Temp, I)));
-               Payload_Index := Payload_Index + 1;
-            end loop;
-            Payloads (Frame_Index) (Min_Ada.Byte (Payload_Index)) :=
-               Min_Ada.Byte (Character'Pos (ASCII.LF));
-            Payload_Index := Payload_Index + 1;
-            Data_Count := Data_Count + 1;
-         end loop;
-         Payload_Indexes (Frame_Index) := Payload_Index;
-         Frame_Index := Frame_Index + 1;
-         Data_Count := 0;
-         Payload_Index := 1;
-      end loop;
-
-      --Send 10 frames
-      Frame_Index := 1;
-      while Frame_Index < Frame_Count + 1 loop
-         if Frame_Index = 1 then
-            Min_Ada.Send_Frame (
-               Context => Context,
-               ID => 6,
-               Payload => Payloads(Frame_Index),
-               Payload_Length => Min_Ada.Byte (Payload_Indexes(Frame_Index) - 1)
-            );
-         else
-            Min_Ada.Send_Frame (
-               Context => Context,
-               ID => 2,
-               Payload => Payloads(Frame_Index),
-               Payload_Length => Min_Ada.Byte (Payload_Indexes(Frame_Index) - 1)
-            );
-         end if;
-         Frame_Index := Frame_Index + 1;
-      end loop;
-
-      Frame_Index := 1;
    end loop;
 
 end Adc_Standalone;
